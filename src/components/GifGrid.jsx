4bacb8ee9af776c7d54e6cc2cdd785dbfae03ed5
juanfmange <juanfmange@gmail.com>
2023-07-08 22:19:24 -0600

    FTR(destructurar objeto de api en front)

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,19 +1,30 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ( { category } ) => {
 
-    const [counter, setCounter] = useState(10);
-    useEffect(() => {    getGifs(category);
-    },[])
+    const [images, setImages] = useState([]);
+    const getImages = async () => {
+        const newImages = await getGifs(category);
+        setImages(newImages);
+    }
+
+    useEffect(() => {
+        getImages(category);
+    }, [])
   
 
     return (
     <>
         <h3>{category}</h3>
-        <h5>{counter}</h5>
-        <button onClick={() => setCounter(counter + 1)}>+1</button>
-       
+       <ol>
+            {
+                images.map(({id, title}) => (
+                    <li key={id}>{title}</li>
+                ))
+            }
+            <li>Item</li>
+       </ol>
     </>
   )
 }
